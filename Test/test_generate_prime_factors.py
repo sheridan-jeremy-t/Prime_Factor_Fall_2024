2cdf39b115c40cbdfea999ad591709cc510a1e9d
Jeremy <tempestj@sheridancollege.ca>
2025-02-10 14:09:58 -0500

    Refactoring test_generate_prime_factors.py

--- Test/test_generate_prime_factors.py
+++ Test/test_generate_prime_factors.py
@@ -5,23 +5,17 @@ def test_not_integer():
     with pytest.raises(ValueError):
         generate_prime_factors("Hello")
 #step 2 return empty list if n == 1
-def test_empty_list_if_one_called():
+def test_input():
     assert generate_prime_factors(1) == []
 #step 3 return [2] if 2 is called
-def test_if_two_called():
     assert generate_prime_factors(2) == [2]
 #step 4 return [3] if 3 is called
-def test_if_three_called():
     assert generate_prime_factors(3) == [3]
 #step 5 return [2,2] if 4 is called
-def test_if_four_called():
     assert generate_prime_factors(4) == [2,2]
 #step 6 return [2,3] if 6 is called
-def test_if_six_called():
     assert generate_prime_factors(6) == [2,3]
 #step 7 return [2,2,2] if 8 is called
-def test_if_eight_called():
     assert generate_prime_factors(8) == [2,2,2]
 #step 8 return [3,3] if 9 is called
-def test_if_nine_called():
     assert generate_prime_factors(9) == [3,3]
