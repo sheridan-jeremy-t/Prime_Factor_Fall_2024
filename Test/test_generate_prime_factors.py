82256e8318d787c79ee129eea051f8605b91baf8
Jeremy <tempestj@sheridancollege.ca>
2025-02-10 13:34:27 -0500

    step 2 empty list if n == 1, failed

--- Test/test_generate_prime_factors.py
+++ Test/test_generate_prime_factors.py
@@ -1,7 +1,9 @@
 import pytest
 from prime import generate_prime_factors
-
+#step 1 ValueError if not integer
 def test_not_integer():
     with pytest.raises(ValueError):
         generate_prime_factors("Hello")
-
+#step 2 return empty list if n == 1
+def test_empty_list_if_one_called():
+    assert generate_prime_factors(1) == 1
