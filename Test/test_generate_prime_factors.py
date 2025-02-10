820b55a2dba5fa9bb42189460489fcccb867d72a
Jeremy <tempestj@sheridancollege.ca>
2025-02-10 13:36:22 -0500

    step 2 empty list if n == 1, passed. Fix assertion in test case from 1 to []

--- Test/test_generate_prime_factors.py
+++ Test/test_generate_prime_factors.py
@@ -6,4 +6,4 @@ def test_not_integer():
         generate_prime_factors("Hello")
 #step 2 return empty list if n == 1
 def test_empty_list_if_one_called():
-    assert generate_prime_factors(1) == 1
+    assert generate_prime_factors(1) == []
