948737dc1495950c530d261cefa64645be3d9357
Jeremy <tempestj@sheridancollege.ca>
2025-02-10 14:13:49 -0500

    Refactoring prime.py. just commented out lines 7-12. everything still passes

--- prime.py
+++ prime.py
@@ -4,12 +4,12 @@ prime.py -- Write the application code here
 def generate_prime_factors(n):
     if not isinstance(n, int):
         raise ValueError("Not an integer")
-    if n == 1:
-        return []
-    if n == 2:
-        return [2]
-    if n == 3:
-        return [3]
+    # if n == 1:
+    #     return []
+    # if n == 2:
+    #     return [2]
+    # if n == 3:
+    #     return [3]
     factors = []
     while n % 2 == 0:
         factors.append(2)
